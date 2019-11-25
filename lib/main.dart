d8e5cfa795270f32c67ef72a3e152ebac35a537f
Henrique La Montagna <henriquelamontagna@gmail.com>
2019-11-25 15:06:16 -0300

    Card de Contato Add; Pag Add Contato Add.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:agenda_contatos_flutter/ui/contact_page.dart';
 import 'package:agenda_contatos_flutter/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: HomePage(),
+      home: ContactPage(),
+      debugShowCheckedModeBanner: false,
     ));
---
+++ lib/ui/contact_page.dart
@@ -0,0 +1,103 @@
+import 'dart:io';
+
+import 'package:agenda_contatos_flutter/helpers/contact_helper.dart';
+import 'package:flutter/material.dart';
+
+class ContactPage extends StatefulWidget {
+  final Contact contact;
+
+  ContactPage({this.contact});
+
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  Contact _editedContact;
+
+  bool _userEdited = false;
+
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.contact == null) {
+      _editedContact = Contact();
+    } else {
+      _editedContact = Contact.fromMap(widget.contact.toMap());
+
+      _nameController.text = _editedContact.nome;
+      _emailController.text = _editedContact.email;
+      _phoneController.text = _editedContact.phone;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_editedContact.nome ?? "Novo Contato"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Icon(Icons.save),
+        backgroundColor: Colors.red,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            GestureDetector(
+              child: Container(
+                width: 140.0,
+                height: 140.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: _editedContact.img != null
+                        ? FileImage(File(_editedContact.img))
+                        : AssetImage("images/person.png"),
+                  ),
+                ),
+              ),
+            ),
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: "Nome"),
+              onChanged: (text){
+                _userEdited = true;
+                setState(() {
+                  _editedContact.nome = text;
+                });
+              },
+            ),
+            TextField(
+              controller:  _emailController,
+              decoration: InputDecoration(labelText: "Email"),
+              onChanged: (text){
+                _userEdited = true;
+                  _editedContact.email = text;
+              },
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextField(
+              controller: _phoneController,
+              decoration: InputDecoration(labelText: "Phone"),
+              onChanged: (text){
+                _userEdited = true;
+                  _editedContact.phone = text;
+
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
